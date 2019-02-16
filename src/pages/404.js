df9e2985959e218c73273fb27f62944d077cc18d
Miha Jamsek <miha_jamsek@windowslive.com>
2019-02-16 22:58:06 +0100

    edited 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import PropTypes from "prop-types";
+import { LayoutComponent } from "../components/layout/layout.component";
+import { FormattedMessage } from "react-intl";
 
-const NotFoundPage = () => (
+/*const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found" />
         <h1>NOT FOUND</h1>
@@ -11,4 +14,32 @@ const NotFoundPage = () => (
     </Layout>
 );
 
-export default NotFoundPage;
+export default NotFoundPage;*/
+
+export default class NotFoundPage extends Component {
+
+    static propTypes = {
+        pageContext: PropTypes.object,
+    };
+
+    render() {
+        const { pageContext } = this.props;
+        const locale = pageContext.locale;
+
+        return (
+            <LayoutComponent locale={locale}>
+                <div className="page-404">
+                    <h1 className="text-center"><FormattedMessage id="not-found.title"/></h1>
+                    <p className="text-center"><FormattedMessage id="not-found.message"/></p>
+                    <p className="text-center">
+                        <FormattedMessage id="not-found.home"/>
+                        <a href="#">
+                            <FormattedMessage id="not-found.link"/>
+                        </a>
+                    </p>
+                </div>
+            </LayoutComponent>
+        );
+    }
+
+}
