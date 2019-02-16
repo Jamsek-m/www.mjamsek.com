69ba5eb178d16968519b7601dc89493626aa8bc2
Miha Jamsek <miha_jamsek@windowslive.com>
2019-02-16 16:09:25 +0100

    added projects page

--- src/pages/projekti/index.js
+++ src/pages/projekti/index.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { LayoutComponent } from "../../components/layout/layout.component";
 import data from "../../content/projects.json";
-import LinkComponent from "../../components/i18n/link.component";
+import { FormattedMessage } from "react-intl";
+import { navigateTo } from "../../components/i18n/navigate-to";
+import emptyThumbnail from "../../assets/images/projects/empty.png";
+
+import "./projekti.page.scss";
+import { projectsMedia } from "../../content/projects-media";
 
 export default class ProjektiPage extends Component {
 
@@ -10,18 +15,35 @@ export default class ProjektiPage extends Component {
         pageContext: PropTypes.object,
     };
 
+    renderProjectThumbnail(projectId) {
+        return projectsMedia[projectId].thumbnail ? projectsMedia[projectId].thumbnail : emptyThumbnail;
+    }
+
     render() {
         const { pageContext: { locale } } = this.props;
         return (
             <LayoutComponent locale={locale}>
 
-                <h1>Projekti</h1>
+                <h1 className="projects-title"><FormattedMessage id="projects.title"/></h1>
 
-                {data.projects.map((projekt, index) => (
-                    <div key={index}>
-                        <LinkComponent to={projekt.path}>
-                            {projekt.translations[locale].name}
-                        </LinkComponent>
+                {data.projects.map((project, index) => (
+                    <div key={index} className="project-item" id={project.id}>
+
+                        <div className="project-name" onClick={() => navigateTo(project.path, locale)}>
+                            <h3>{project.translations[locale].name}</h3>
+                        </div>
+                        <div className="project-about">
+                            <div className="project-img" onClick={() => navigateTo(project.path, locale)}>
+                                <img src={this.renderProjectThumbnail(project.id)}/>
+                            </div>
+                            <div className="project-desc" onClick={() => navigateTo(project.path, locale)}>
+                                {project.translations[locale].shortDescription}
+                            </div>
+                        </div>
+
+                        {/*<LinkComponent to={project.path}>*/}
+                        {/*{project.translations[locale].name}*/}
+                        {/*</LinkComponent>*/}
                     </div>
                 ))}
 
