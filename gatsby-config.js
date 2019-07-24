3ee2ec040162e26bddac5f618fd71718ae390f81
Miha Jamsek <miha_jamsek@windowslive.com>
2019-07-24 12:44:48 +0200

    added robots and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
 module.exports = {
     siteMetadata: {
-        title: "Gatsby Default Starter",
-        description: "Kick off your next, great Gatsby project with this " +
-            "default starter. This barebones starter ships with the main Gatsby " +
-            "configuration files you might need.",
-        author: "@gatsbyjs",
+        title: "mJamsek Site",
+        description: "mJamsek personal site",
+        author: "Miha Jamsek",
+        siteUrl: "https://www.mjamsek.com"
     },
     plugins: [
         "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-robots-txt",
         {
             resolve: "gatsby-plugin-sass",
             options: {
