aed6ef087f9528ec71f6f5b634a6f6a1f089e4df
Miha Jamsek <miha_jamsek@windowslive.com>
2021-03-13 22:48:08 +0100

    styled 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,38 @@
 import React from "react";
-import { Layout, Seo } from "../components";
+import { Layout, PageContainer, Seo } from "../components";
+import { graphql } from "gatsby";
+import { useTranslation } from "react-i18next";
+
+import {container} from "./404.module.scss";
 
 const NotFoundPage = () => {
+    const { t } = useTranslation();
+    
     return (
         <Layout>
             <Seo title={"404: Not found"}/>
-            <div>
-                <h1>404 Error!</h1>
-                <p>Requested page not found!</p>
-            </div>
+            <PageContainer>
+                <div className={container}>
+                    <h1>{t("common:404.title")}</h1>
+                    <p>{t("common:404.message")}</p>
+                </div>
+            </PageContainer>
         </Layout>
     );
 };
 
 export default NotFoundPage;
+
+export const query = graphql`
+    query($language: String!) {
+        locales: allLocale(filter: {language: {eq: $language}}) {
+            edges {
+                node {
+                    ns
+                    data
+                    language
+                }
+            }
+        }
+    }
+`;
